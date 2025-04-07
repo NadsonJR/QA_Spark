4d2e4629b1c3ed318c966853263d2c1630ba5cf3
acruzgai <nadsonjunior@live.com>
2025-04-07 11:53:11 -0300

    refactor: remove unnecessary log statements in Utilities for cleaner code

--- src/test/java/org/desafio/utils/Utilities.java
+++ src/test/java/org/desafio/utils/Utilities.java
@@ -54,7 +54,6 @@ public class Utilities {
         TestData() {
             screenshots = new ArrayList<>();
             apiResponses = new ArrayList<>();
-            log.info("New TestData instance created");
         }
     }
 
@@ -106,7 +105,6 @@ public class Utilities {
             File tempDir = new File(TEMP_DIR);
             if (!tempDir.exists()) {
                 boolean created = tempDir.mkdirs();
-                log.info("Temp directory created: " + created);
             }
             File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
             String tempPath = TEMP_DIR + UUID.randomUUID() + "_" + fileName + BaseConfig.SCREENSHOT_TYPE;
@@ -114,7 +112,6 @@ public class Utilities {
             FileUtils.copyFile(screenshot, tempFile);
             String stepName = fileName.split("\\.")[0];
             currentTest.screenshots.add(new ScreenshotInfo(stepName, tempPath));
-            log.info("Screenshot added: " + tempPath);
             attachPageScreenshot(driver, stepName);
         } catch (Exception e) {
             log.error("Error in takeScreenshot", e);
@@ -228,7 +225,6 @@ public class Utilities {
             }
             // Close the document
             document.close();
-            log.info("Document generated successfully");
             // Cleanup
             cleanup();
         } catch (Exception e) {
@@ -257,7 +253,6 @@ public class Utilities {
             int statusCode = response.getStatusCode();
             String responseBody = response.getBody().asString();
             currentTest.apiResponses.add(new APIResponseInfo(stepName, statusCode, responseBody));
-            log.info("API response added: " + stepName);
         } catch (Exception e) {
             log.error("Error adding API response", e);
         }
